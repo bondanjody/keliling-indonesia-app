41a2da27a7c9f732d16e188dda969beef6ce4757
bondanjody <bondanjodys@gmail.com>
2023-04-13 17:30:42 +0700

    Menambahkan function old() pada form edit supaya menyimpan data terakhir (menampilkan data terakhir dari session field tersebut)

--- app/Views/wisata/edit.php
+++ app/Views/wisata/edit.php
@@ -16,37 +16,37 @@
                     <tr>
                         <td>
                             <label for="namatempat" style="font-size: small;color:#555">Nama tempat</label><br>
-                            <input type="text" name="namatempat" id="namatempat" style="width:100%; height:2rem; padding:0.3rem" value="<?= $wisata['namatempat']; ?>" autofocus required>
+                            <input type="text" name="namatempat" id="namatempat" style="width:100%; height:2rem; padding:0.3rem" value="<?= (old('namatempat')) ? old('namatempat') : $wisata['namatempat']; ?>" autofocus required>
                         </td>
                     </tr>
                     <tr>
                         <td>
                             <label for="tentang" style="font-size: small;color:#555">Tentang</label><br>
-                            <textarea type="text" name="tentang" id="tentang" style="width:100%; height:3rem; padding:0.3rem" required><?= $wisata['tentang']; ?></textarea>
+                            <textarea type="text" name="tentang" id="tentang" style="width:100%; height:3rem; padding:0.3rem" required><?= (old('tentang')) ? old('tentang') : $wisata['tentang']; ?></textarea>
                         </td>
                     </tr>
                     <tr>
                         <td>
                             <label for="harga" style="font-size: small;color:#555">Harga</label><br>
-                            <input type="text" name="harga" id="harga" style="width:100%; height:2rem; padding:0.3rem" value="<?= $wisata['harga']; ?>" required>
+                            <input type="text" name="harga" id="harga" style="width:100%; height:2rem; padding:0.3rem" value="<?= (old('harga')) ? old('harga') : $wisata['harga']; ?>" required>
                         </td>
                     </tr>
                     <tr>
                         <td>
                             <label for="provinsi" style="font-size: small;color:#555">Provinsi</label><br>
-                            <input type="text" name="provinsi" id="provinsi" style="width:100%; height:2rem; padding:0.3rem" value="<?= $wisata['provinsi']; ?>">
+                            <input type="text" name="provinsi" id="provinsi" style="width:100%; height:2rem; padding:0.3rem" value="<?= (old('provinsi')) ? old('provinsi') : $wisata['provinsi']; ?>">
                         </td>
                     </tr>
                     <tr>
                         <td>
                             <label for="alamat" style="font-size: small;color:#555" required>Alamat (Google Maps API)</label><br>
-                            <textarea name="alamat" id="alamat" style="width:100%; height:3rem; padding:0.3rem" required><?= $wisata['alamat']; ?></textarea>
+                            <textarea name="alamat" id="alamat" style="width:100%; height:3rem; padding:0.3rem" required><?= (old('provinsi')) ? old('provinsi') : $wisata['provinsi']; ?></textarea>
                         </td>
                     </tr>
                     <tr>
                         <td>
                             <label for="provinsi" style="font-size: small;color:#555">Gambar</label><br>
-                            <input type="text" name="gambar" id="gambar" style="width:100%; height:2rem; padding:0.3rem" value="<?= $wisata['gambar']; ?>" required>
+                            <input type="text" name="gambar" id="gambar" style="width:100%; height:2rem; padding:0.3rem" value="<?= (old('gambar')) ? old('gambar') : $wisata['gambar']; ?>" required>
                         </td>
                     </tr>
                     <tr>
