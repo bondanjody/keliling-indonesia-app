16084889206c121b43516d4398818b82d5f10e25
bondanjody <bondanjodys@gmail.com>
2023-04-15 03:17:49 +0700

    Menggunakan Faker. Mengatur data negara Faker. Melakukan perulangan untuk memasukkan data ke database.

--- app/Database/Seeds/OrangSeeds.php
+++ app/Database/Seeds/OrangSeeds.php
@@ -9,31 +9,42 @@ class OrangSeeds extends Seeder
 {
     public function run()
     {
-        $data = [
-            [
-                'nama' => 'Lionel Jody',
-                'alamat'    => 'Jl. Arjuna, Bulukerto',
-                'created_at' => Time::now(),
-                'updated_at' => Time::now(),
-            ],
-            [
-                'nama' => 'Alexander Jody',
-                'alamat'    => 'Jl. Werkudara, Bulukerto',
-                'created_at' => Time::now(),
-                'updated_at' => Time::now(),
-            ],
-            [
-                'nama' => 'Hector Jody',
-                'alamat'    => 'Jl. Nakula, Bulukerto',
-                'created_at' => Time::now(),
-                'updated_at' => Time::now(),
-            ]
-        ];
+        // $data = [
+        //     [
+        //         'nama' => 'Lionel Jody',
+        //         'alamat'    => 'Jl. Arjuna, Bulukerto',
+        //         'created_at' => Time::now(),
+        //         'updated_at' => Time::now(),
+        //     ],
+        //     [
+        //         'nama' => 'Alexander Jody',
+        //         'alamat'    => 'Jl. Werkudara, Bulukerto',
+        //         'created_at' => Time::now(),
+        //         'updated_at' => Time::now(),
+        //     ],
+        //     [
+        //         'nama' => 'Hector Jody',
+        //         'alamat'    => 'Jl. Nakula, Bulukerto',
+        //         'created_at' => Time::now(),
+        //         'updated_at' => Time::now(),
+        //     ]
+        // ];
+
+        $faker = \Faker\Factory::create('id_ID');
+        for ($i = 0; $i <= 100; $i++) {
+            $data = [
+                'nama' => $faker->name,
+                'alamat' => $faker->address,
+                'created_at' => Time::createFromTimestamp($faker->unixTime()),
+                'updated_at' => Time::now()
+            ];
+            $this->db->table('orang')->insert($data);
+        }
 
         // Simple Queries
         // $this->db->query('INSERT INTO orang (nama, alamat, created_at, updated_at) VALUES(:nama:, :alamat:, :created_at:, :updated_at:)', $data);
 
         // Using Query Builder
-        $this->db->table('orang')->insertBatch($data);
+        // $this->db->table('orang')->insertBatch($data);
     }
 }
